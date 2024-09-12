7f690e85b98876731eba628935d96bcaa5dbfea2
Bruno <78873048+BrunoSantosCode@users.noreply.github.com>
2024-09-12 14:21:41 +0100

    Add files via upload

---
+++ WAREHOUSE.py
@@ -0,0 +1,183 @@
+"""
+This block gives as output:
+ - three boolean variables indicating the presence of pieces to unload [UNLOAD_{1,2,3}]
+"""
+
+from ultralytics import YOLO
+import numpy as np
+import cv2
+
+
+class WAREHOUSE:
+
+    def __init__(self):
+        self.camera_name = ""
+        self.network_name = ""
+        self.unload_1 = 0
+        self.unload_2 = 0
+        self.unload_3 = 0
+        self.display_img = True
+
+    def schedule(self, event_name, event_value, camera_name, network_name):    
+        if event_name == 'INIT':
+            if (not camera_name) or (not network_name):
+                print("Error, camera or network name not specified")
+            else:
+                # Open camera
+                self.camera_name = camera_name
+                self.camera = ZED(camera_name)
+                print("Camera OK")
+                # Init YOLOv8
+                self.network_name = network_name
+                self.yolo = YOLOv8(network_name)
+                print("YOLO OK")
+            return [event_value, event_value, self.unload_1, self.unload_2, self.unload_3]
+
+        elif event_name == 'READ':
+            # Acquire camera image
+            img = self.camera.capture()
+            # Detect components
+            self.yolo.detect(img)
+            objects = self.yolo.get_objects()
+            # Analyse detected components
+            analyser = ObjectsAnalyser(img, objects)
+            # Check components
+            self.unload_1 = analyser.is_ready_to_remove("upper")
+            self.unload_2 = analyser.is_ready_to_remove("middle")
+            self.unload_3 = analyser.is_ready_to_remove("bottom")
+            # Display image
+            if self.display_img == True:
+                analyser.draw_circles()
+                show_img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+                cv2.imshow("ZED", show_img)
+                if cv2.waitKey(1) == ord('q'):
+                    cv2.destroyAllWindows()
+                    self.display_img = False
+
+            return [None, event_value, self.unload_1, self.unload_2, self.unload_3]
+                    
+
+# ZED Camera Class
+class ZED():
+
+    def __init__(self, device):
+        # Create a Camera object
+        self.cam = cv2.VideoCapture(device)
+        # Set resolution
+        self.camera_width = 1920
+        self.camera_height = 1080
+        self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.camera_width*2)
+        self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.camera_height)
+        # Test camera
+        ret, _ = self.cam.read()
+        if not ret:
+            return -1
+
+
+    def capture(self):
+        # Capture
+        ret, frame = self.cam.read()
+        if not ret:
+            return -1
+        # Keep only left side (for ZED)
+        left_image = frame[:, :self.camera_width]
+        # ROI
+        left_image = left_image[200:1000, 620:1500]
+        return left_image
+        
+    def close(self):
+        self.zed.close()
+
+
+# YOLO Neural Network Class
+class YOLOv8():
+
+    def __init__(self, model):
+        # Init YOLO
+        self.model = YOLO(model, task="segment")
+        self.results = None
+
+    def detect(self, image):
+        # Detect objects
+        self.results = self.model.predict(image, conf=0.75, device="cuda")
+        # Analyse results from YOLOv8
+        for result in self.results:
+            result.cpu().numpy()
+            detected_objects = result.__len__()
+            if detected_objects == 0:
+                return 0
+        return detected_objects
+
+    def get_objects(self):
+        objects = []
+        for result in self.results:
+            result.cpu().numpy()
+            detected_objects = result.__len__()
+            for i in range(detected_objects):
+                # Get object centroid
+                mask_pixels = np.array(result.masks.xy[i], dtype=np.int32)
+                M = cv2.moments(mask_pixels)
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])  
+                # Get object mask
+                cls = result.boxes.data[i, -1].cpu().numpy()
+                class_label = result.names[int(cls)]
+                # Add object to dictionay
+                objects.append({
+                    "class": class_label,
+                    "centroid": (cX, cY)
+                })
+        return objects
+        
+# Objects Analyser Class
+class ObjectsAnalyser():
+
+    def __init__(self, image, objects):
+        self.image = image
+        self.objects = objects
+        self.color_map = {
+            "metal": (192, 192, 192),
+            "black": (0, 0, 0),
+            "orange": (0, 165, 255)
+        }
+        self.color_zone = {
+            "metal": "upper",
+            "black": "middle",
+            "orange": "bottom"
+        }
+
+    def draw_circles(self):
+        # Draw a circle around each detected component
+        for obj in self.objects:
+            class_name = obj["class"]
+            centroid = obj["centroid"]
+            color = self.color_map[class_name]
+            cv2.circle(self.image, centroid, 75, color, 10)
+
+    def number_of(self, class_name):
+        # Count the number of objects of a specific class
+        count = sum(1 for obj in self.objects if obj["class"] == class_name)
+        return count
+    
+    def is_ready_to_remove(self, zone_name):
+        height = self.image.shape[0]
+        third_height = height // 3
+        zones = {
+            "upper": (0, third_height),
+            "middle": (third_height, 2 * third_height),
+            "bottom": (2 * third_height, height)
+        }
+        min_y, max_y = zones[zone_name]
+        rightmost_obj_color = ""
+        rightmost_x = -1
+        for obj in self.objects:
+            obj_x, obj_y = obj["centroid"]
+            if min_y < obj_y < max_y:
+                if obj_x > rightmost_x:
+                    rightmost_x = obj_x
+                    rightmost_obj_color = obj["class"]
+        if (rightmost_x >= 770):
+            # Return the position of the class in the color_zone dictionary
+            return list(self.color_zone.keys()).index(rightmost_obj_color) + 1
+        else:
+            return 0
---
+++ XARM5_ROBOT.py
@@ -0,0 +1,345 @@
+"""
+This block takes as input:
+ - xArm5 IP [ROBOT_IP]
+ - boolean variable indicating the presence of a component [OPCUA_GRAB] 
+ - three boolean variables indicating the presence of a piece to unload [UNLOAD_{1,2,3}]
+If OPCUA_GRAB is True the robot starts the pick-and-place movement,
+transporting the wood piece from the Processing and Validation Station to the Sorting Station.
+If UNLOAD_X is True the robot starts the unload warehouse movement,
+transporting the wood piece from the Sorting Station to the conveyor.
+"""
+
+import time
+import traceback
+from xarm import version
+from xarm.wrapper import XArmAPI
+
+PLUS_OFFSET = 55
+
+# xArm5 Poses (UNLOAD)
+PICK_POSE = [-585,-2,177,180,0,-180]
+PICK_POSE_PLUS = PICK_POSE[:]
+PICK_POSE_PLUS[2] += PLUS_OFFSET
+
+MIDDLE_POSE = [0,-325.5,207.5,180,0,-90]
+
+PLACE_POSE = [435,4,135,180,0,6.3]
+PLACE_POSE_PLUS = PLACE_POSE[:]
+PLACE_POSE_PLUS[2] += PLUS_OFFSET
+
+# xArm5 Poses (SORTING)
+START_POSE = [0, -325.5, 207.5, 180, 0, -90]
+START_POSE_JOINTS = [-90.0, -27.2, -31.1, 58.3, 0.0]
+
+PICK_POSE_1 = [455.9, 254.4, 76.0, 180, 0, 90]
+PICK_POSE_2 = [541.4, 254.7, 76.5, 180, 0, 90]
+PICK_POSE_3 = [627.7, 255.9, 74.0, 180, 0, 90]
+
+MIDDLE_POSE_SORTING = [325.7, 0, 200, 180, 0, 0]
+
+PLACE_POSE_1 = [-125.9, 437.6, 1, 180, 0, 90]
+PLACE_POSE_2 = [-125.9, 497.6, 1, 180, 0, 90]
+PLACE_POSE_3 = [-125.9, 557.6, 1, 180, 0, 90]
+
+# xArm5 Poses (REPLACE)
+MIDDLE_POSE_REPLACE = [543.7, 159.2, 138.0, 180, 0, 90]
+
+REPLACE_POSE_1 = [462.1, 59.2, 131.4, 180, 0, 90]
+REPLACE_POSE_2 = [544.0, 59.2, 131.4, 180, 0, 90]
+REPLACE_POSE_3 = [624.8, 59.2, 139.6, 180, 0, 90]
+
+
+class XARM5_ROBOT:
+
+    def __init__(self):
+        self.robot_ip = ""
+
+    def schedule(self, event_name, event_value, robot_ip, opcua_grab, unload_1, unload_2, unload_3):            
+
+        if event_name == 'INIT':
+            if not robot_ip:
+                print("Error, xArm5 ip not specified")
+            else:
+                self.robot_ip = robot_ip
+                RobotMain.pprint('xArm-Python-SDK Version:{}'.format(version.__version__))
+                self.arm = XArmAPI(self.robot_ip, baud_checkset=False)
+                self.robot_main = RobotMain(self.arm)
+                self.robot_main.config()
+            return [event_value, event_value]
+
+        elif event_name == 'READ':
+            # From Validation Station to Sorting Station
+            if opcua_grab:
+                self.robot_main.pick_and_place() 
+            # From Sorting Station to Sorting Station (Rearrange)
+            elif unload_1 == 2:
+                self.robot_main.unload_piece(PICK_POSE_1, MIDDLE_POSE_REPLACE, REPLACE_POSE_2)
+            elif unload_1 == 3:
+                self.robot_main.unload_piece(PICK_POSE_1, MIDDLE_POSE_REPLACE, REPLACE_POSE_3)
+            elif unload_2 == 1:
+                self.robot_main.unload_piece(PICK_POSE_2, MIDDLE_POSE_REPLACE, REPLACE_POSE_1)
+            elif unload_2 == 3:
+                self.robot_main.unload_piece(PICK_POSE_2, MIDDLE_POSE_REPLACE, REPLACE_POSE_3)
+            elif unload_3 == 1:
+                self.robot_main.unload_piece(PICK_POSE_3, MIDDLE_POSE_REPLACE, REPLACE_POSE_1)
+            elif unload_3 == 2:
+                self.robot_main.unload_piece(PICK_POSE_3, MIDDLE_POSE_REPLACE, REPLACE_POSE_2)
+            # From Sorting Station to Conveyor
+            elif unload_1 == 1:
+                self.robot_main.unload_piece(PICK_POSE_1, MIDDLE_POSE_SORTING, PLACE_POSE_1)
+            elif unload_2 == 2:
+                self.robot_main.unload_piece(PICK_POSE_2, MIDDLE_POSE_SORTING, PLACE_POSE_2)
+            elif unload_3 == 3:
+                self.robot_main.unload_piece(PICK_POSE_3, MIDDLE_POSE_SORTING, PLACE_POSE_3)
+                        
+            return [None, event_value]
+                    
+
+
+class RobotMain(object):
+    """Robot Main Class"""
+    def __init__(self, robot, **kwargs):
+        self.alive = True
+        self._arm = robot
+        self._ignore_exit_state = False
+        # SET ROBOT SPEED HERE #
+        self._tcp_speed = 350     # MAX =  1000 mm/s
+        self._tcp_acc = 1000      # MAX = 50000 mm/s²
+        self._angle_speed = 180   # MAX =  180 degrees/s
+        self._angle_acc = 500     # MAX = 1145 degrees/s²   
+        # # # # # # # # # # # # #
+        self._vars = {}
+        self._funcs = {}
+        self._robot_init()
+
+    # Robot init
+    def _robot_init(self):
+        self._arm.clean_warn()
+        self._arm.clean_error()
+        self._arm.motion_enable(True)
+        self._arm.set_mode(0)
+        self._arm.set_state(0)
+        time.sleep(1)
+        self._arm.register_error_warn_changed_callback(self._error_warn_changed_callback)
+        self._arm.register_state_changed_callback(self._state_changed_callback)
+        if hasattr(self._arm, 'register_count_changed_callback'):
+            self._arm.register_count_changed_callback(self._count_changed_callback)
+
+    # Register error/warn changed callback
+    def _error_warn_changed_callback(self, data):
+        if data and data['error_code'] != 0:
+            self.alive = False
+            self.pprint('err={}, quit'.format(data['error_code']))
+            self._arm.release_error_warn_changed_callback(self._error_warn_changed_callback)
+
+    # Register state changed callback
+    def _state_changed_callback(self, data):
+        if not self._ignore_exit_state and data and data['state'] == 4:
+            self.alive = False
+            self.pprint('state=4, quit')
+            self._arm.release_state_changed_callback(self._state_changed_callback)
+
+    # Register count changed callback
+    def _count_changed_callback(self, data):
+        if self.is_alive:
+            self.pprint('counter val: {}'.format(data['count']))
+
+    def _check_code(self, code, label):
+        if not self.is_alive or code != 0:
+            self.alive = False
+            ret1 = self._arm.get_state()
+            ret2 = self._arm.get_err_warn_code()
+            self.pprint('{}, code={}, connected={}, state={}, error={}, ret1={}. ret2={}'.format(label, code, self._arm.connected, self._arm.state, self._arm.error_code, ret1, ret2))
+        return self.is_alive
+
+    @staticmethod
+    def pprint(*args, **kwargs):
+        try:
+            stack_tuple = traceback.extract_stack(limit=2)[0]
+            print('[{}][{}] {}'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), stack_tuple[1], ' '.join(map(str, args))))
+        except:
+            print(*args, **kwargs)
+
+    @property
+    def arm(self):
+        return self._arm
+
+    @property
+    def VARS(self):
+        return self._vars
+
+    @property
+    def FUNCS(self):
+        return self._funcs
+
+    @property
+    def is_alive(self):
+        if self.alive and self._arm.connected and self._arm.error_code == 0:
+            if self._ignore_exit_state:
+                return True
+            if self._arm.state == 5:
+                cnt = 0
+                while self._arm.state == 5 and cnt < 5:
+                    cnt += 1
+                    time.sleep(0.1)
+            return self._arm.state < 4
+        else:
+            return False
+        
+    # Set Robot configurations
+    def config(self):
+        try:
+            # Set xArm5 configurations
+            code = self._arm.set_tcp_load(0.61, [0, 0, 53])
+            if not self._check_code(code, 'set_tcp_load'): return
+
+            code = self._arm.set_tcp_offset([0, 0, 126, 0, 0, 0], wait=True)
+            self._arm.set_state(0)
+            if not self._check_code(code, 'set_tcp_offset'): return
+
+            time.sleep(0.5)
+
+            code = self._arm.set_world_offset([0, 0, 0, 0, 0, 0])
+            self._arm.set_state(0)
+            if not self._check_code(code, 'set_world_offset'): return
+
+            time.sleep(0.5)
+
+            current_angle = self._arm.angles
+            angle = -(current_angle[1] + current_angle[2])
+            code = self._arm.set_servo_angle(servo_id=4, angle=angle)
+            if not self._check_code(code, 'set_end_level'): return
+
+            # Moving xArm5 to initial position
+            self.go_to_initial_pose()
+
+        except Exception as e:
+            self.pprint('MainException: {}'.format(e))
+
+
+    # Robot Main Run
+    def pick_and_place(self):
+        try:   
+            # Moving xArm5 to initial position
+            code = self._arm.set_position(*START_POSE, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=False)
+            if not self._check_code(code, 'set_position'): return
+
+            # Picking
+            code = self._arm.set_position(*PICK_POSE_PLUS, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=False)
+            if not self._check_code(code, 'set_position'): return
+
+            code = self._arm.set_position(*PICK_POSE, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=-1, wait=True)
+            if not self._check_code(code, 'set_position'): return
+
+            code = self._arm.set_suction_cup(True, wait=False, delay_sec=0)
+            if not self._check_code(code, 'set_suction_cup'): return
+
+            # Check if picked
+            if self._arm.arm.check_air_pump_state(1, timeout=2.0):
+                print("Object picked")
+
+                # Placing
+                code = self._arm.set_position(*PICK_POSE_PLUS, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=False)
+                if not self._check_code(code, 'set_position'): return
+
+                code = self._arm.set_position(*MIDDLE_POSE, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=250, wait=False)
+                if not self._check_code(code, 'set_position'): return
+
+                code = self._arm.set_position(*PLACE_POSE_PLUS, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=False)
+                if not self._check_code(code, 'set_position'): return
+
+                code = self._arm.set_position(*PLACE_POSE, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=-1, wait=True)
+                if not self._check_code(code, 'set_position'): return
+
+                code = self._arm.set_suction_cup(False, wait=False, delay_sec=0)
+                if not self._check_code(code, 'set_suction_cup'): return
+
+                code = self._arm.set_position(*PLACE_POSE_PLUS, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=False)
+                if not self._check_code(code, 'set_position'): return
+
+            else:
+                print("Object not picked")
+                
+                # Returning to initial position
+                code = self._arm.set_suction_cup(False, wait=False, delay_sec=0)
+                if not self._check_code(code, 'set_suction_cup'): return
+                
+                code = self._arm.set_position(*PICK_POSE_PLUS, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=False)
+                if not self._check_code(code, 'set_position'): return
+
+            code = self._arm.set_position(*MIDDLE_POSE, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=-1, wait=True)
+            if not self._check_code(code, 'set_position'): return  
+
+        except Exception as e:
+            self.pprint('MainException: {}'.format(e))
+
+
+    def unload_piece(self, pick_pose, middle_pose, place_pose):
+        try:            
+            # Start control
+            if self.is_alive:
+                pick_pose_plus = pick_pose[:]
+                pick_pose_plus[2] += PLUS_OFFSET
+                place_pose_plus = place_pose[:]
+                place_pose_plus[2] += PLUS_OFFSET
+                # Picking
+                code = self._arm.set_position(*pick_pose_plus, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=False)
+                if not self._check_code(code, 'set_position'): return
+
+                code = self._arm.set_position(*pick_pose, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=True)
+                if not self._check_code(code, 'set_position'): return
+
+                code = self._arm.set_suction_cup(True, wait=False, delay_sec=0)
+                if not self._check_code(code, 'set_suction_cup'): return
+
+                # Check if picked
+                if self._arm.arm.check_air_pump_state(1, timeout=2.0):
+                    print("Object picked")
+
+                    # Placing
+                    code = self._arm.set_position(*pick_pose_plus, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=False)
+                    if not self._check_code(code, 'set_position'): return
+
+                    code = self._arm.set_position(*place_pose_plus, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=False)
+                    if not self._check_code(code, 'set_position'): return
+
+                    code = self._arm.set_position(*place_pose, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=-1, wait=True)
+                    if not self._check_code(code, 'set_position'): return
+
+                    code = self._arm.set_suction_cup(False, wait=False, delay_sec=0)
+                    if not self._check_code(code, 'set_suction_cup'): return
+
+                    code = self._arm.set_position(*place_pose_plus, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=False)
+                    if not self._check_code(code, 'set_position'): return
+
+                else:
+                    print("Object not picked")
+                    
+                    # Returning to initial position
+                    code = self._arm.set_suction_cup(False, wait=False, delay_sec=0)
+                    if not self._check_code(code, 'set_suction_cup'): return
+                    
+                    code = self._arm.set_position(*pick_pose_plus, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=25, wait=False)
+                    if not self._check_code(code, 'set_position'): return
+                
+                code = self._arm.set_position(*MIDDLE_POSE_SORTING, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=200, wait=False)
+                if not self._check_code(code, 'set_position'): return
+                
+        except Exception as e:
+            self.pprint('MainException: {}'.format(e))
+
+
+    def go_to_initial_pose(self):
+        # Moving xArm5 to initial position
+        code = self._arm.set_position(*START_POSE, speed=self._tcp_speed, mvacc=self._tcp_acc, radius=-1, wait=True)
+        if not self._check_code(code, 'set_position'): return
+        code = self._arm.set_servo_angle(angle=START_POSE_JOINTS, speed=self._angle_speed, mvacc=self._angle_acc, wait=True, radius=-1)
+        if not self._check_code(code, 'set_servo_angle'): return
+
+
+    def stop(self):
+        self.alive = False
+        self._arm.release_error_warn_changed_callback(self._error_warn_changed_callback)
+        self._arm.release_state_changed_callback(self._state_changed_callback)
+        if hasattr(self._arm, 'release_count_changed_callback'):
+            self._arm.release_count_changed_callback(self._count_changed_callback)
